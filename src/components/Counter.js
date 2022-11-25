1603bccec11801c8a523a21e0fbe5c7601a1c710
anjali790 <anjaliruby790@gmail.com>
2022-11-25 19:56:41 +0530

    redux

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import './Counter.css';
 
 export function Counter() {
     const count = useSelector((state) => {
@@ -8,9 +9,11 @@ export function Counter() {
 
     return (
         <>
-            <h1>{count}</h1>
-            <button>Increment</button>
-            <button>Decrement</button>
+            <div className='counter_div'>
+                <h1>{count}</h1>
+                <button className='button'>Increment</button>
+                <button className='button'>Decrement</button>
+            </div>
         </>
     );
 };
